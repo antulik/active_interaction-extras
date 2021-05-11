dfae8179121b93f9aa3d164cc4ad0be2f1660429
Anton Katunin <antulik@gmail.com>
2021-05-11 15:07:45 +1000

    Load all filter extension in specs

---
+++ lib/active_interaction/extras/filter_extensions.rb
@@ -0,0 +1,2 @@
+require 'active_interaction/extras/filter_extensions/auto_strip_hash'
+require 'active_interaction/extras/filter_extensions/multi_object'
--- spec/support/testable_service.rb
+++ spec/support/testable_service.rb
@@ -1,4 +1,5 @@
 require 'active_interaction/active_job'
+require 'active_interaction/extras/filter_extensions'
 
 class TestableService < ActiveInteraction::Base
   include ActiveInteraction::Extras::All
