d0a9f05865149741e54b96e32d38b17fd9927dce
Anton Katunin <antulik@gmail.com>
2025-03-16 18:02:04 +1100

    Add ModelNames

--- lib/active_interaction/extras.rb
+++ lib/active_interaction/extras.rb
@@ -45,6 +45,26 @@ module ActiveInteraction
       end
     end
 
+    concern :ModelNames do
+      class_methods do
+        def inherited(subclass)
+          super.tap { subclass.set_model_naming }
+        end
+
+        def set_model_naming
+          str = name.deconstantize.underscore
+          model_name.route_key = str.pluralize.to_sym
+          model_name.singular_route_key = str.singularize.to_sym
+        end
+
+        def singular_resource_route_key!
+          str = name.deconstantize.underscore
+          model_name.route_key = str.singularize.to_sym
+        end
+      end
+    end
+
+
     concern :AfterInitialize do
       include ActiveSupport::Callbacks
 
